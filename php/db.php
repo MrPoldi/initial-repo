8f3cb1b89426ec37c631ac4e37aab731124c515b
MrPoldi <40165691+MrPoldi@users.noreply.github.com>
2020-01-06 14:50:39 +0100

    łączenie do zdalnej bazy
    Zmiana w connection stringu z localhosta na zdalną bazę danych

--- php/db.php
+++ php/db.php
@@ -2,5 +2,5 @@
     ini_set('display_errors',1);
     error_reporting(E_ALL);
     header("Access-Control-Allow-Origin: *");
-    $con = mysqli_connect("localhost:3306","test","test","bikol") or die ("Error: could not connect to database " . $con -> connect_error);
+    $con = mysqli_connect("remotemysql.com","SF4j7jn2Aj","gsszmwolkM","SF4j7jn2Aj") or die ("Error: could not connect to database " . $con -> connect_error);
 ?>
