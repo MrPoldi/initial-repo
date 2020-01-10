950c987f1ba4f344ec11ec2b006142453566272e
Kamil Guttmann <kamgut@st.amu.edu.pl>
2020-01-10 14:23:34 +0100

    Normalizacja numeru telefonu i sprawdzenie haseł

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,13 +1,21 @@
 $(document).ready(function(){
   $("#login-button").click(function(){      
       var phoneNumber = $("#phonenumber").val();
+      phoneNumber.replace(/\s+/g, '');
+      phoneNumber.replace('+','');
+      
       var fullName = $("#userfullname").val();
       var spl = fullName.split(" ");
       var firstName = spl[0];
       var lastName = spl[1];
+      
       var username = $("#username").val();
       var password = $("#password").val();
       var passwordConfirm = $("#password_confirm").val();
+      if(password != passwordConfirm){
+          alert("Hasła się nie zgadzają!")
+          return false;
+      }
       var availability = $("#availability").val();
       
       var dataString = "phone=" + phoneNumber + "&firstName=" + firstName + "&lastName=" + lastName + "&username=" + username + "&password=" + password + "&availability=" + availability + "&insert=";
