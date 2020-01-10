15275102ea2355055963956ec9f6c462db6d8169
Wojciech Łukasik <wojtek.lukasik@yahoo.pl>
2020-01-10 22:02:05 +0100

    sprawdzanie danych w rejestracji

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $("#login-button").click(function(){      
+  $("#login-button").click(function(){    
       var phoneNumber = $("#phonenumber").val();
       var fullName = $("#userfullname").val();
       var spl = fullName.split(" ");
@@ -10,28 +10,63 @@ $(document).ready(function(){
       var passwordConfirm = $("#password_confirm").val();
       var availability = $("#availability").val();
       
-      var dataString = "phone=" + phoneNumber + "&firstName=" + firstName + "&lastName=" + lastName + "&username=" + username + "&password=" + password + "&availability=" + availability + "&insert=";
+      var correctPhoneNumberRegex = '^([+]\d{9,}|\d{9,})';
+      var correctAvailRegex = '^([01]\d|2[0-3]):([0-5]\d) - ([01]\d|2[0-3]):([0-5]\d)$';
+
+      var dataString;
       
-      //alert(dataString);
-      
-      $.ajax({
-            type: "POST",
-            url: "http://localhost/bikol/insert.php",
-            data: dataString,
-            crossDomain: true,
-            cache: false,
-            beforeSend: function() {
-                $("#login-button").val('Przesyłam...');
-            },
-            success: function(data) {
-                if (data == "success") {
-                    alert("inserted");
-                    $("#login-button").val('Zatwierdź');
-                } 
-                else if (data == "error") {
-                       alert("error");
+      if(phoneNumber.val().match(correctPhoneNumberRegex)){
+        // numer jest git
+        if(password.val().length == 0 || passwordConfirm.val().length == 0){
+          // puste hasło
+          alert("Password is empty!");
+        }else if(password.val() != passwordConfirm.val()){
+          // różne hasła
+          alert("Passwords don't match")
+        }else{
+          // hasła są git
+          if(availability.val().match(correctAvailRegex)){
+            // godziny są git
+            dataString = "phone=" + phoneNumber + "&firstName=" + firstName + "&lastName=" + lastName + "&username=" + username + "&password=" + password + "&availability=" + availability + "&insert=";      
+          //alert(dataString);      
+          $.ajax({
+                type: "POST",
+                url: "http://localhost/bikol/insert.php",
+                data: dataString,
+                crossDomain: true,
+                cache: false,
+                beforeSend: function() {
+                    $("#login-button").val('Przesyłam...');
+                },
+                success: function(data) {
+                    if (data == "success") {
+                        alert("inserted");
+                        $("#login-button").val('Zatwierdź');
+                    } 
+                    else if (data == "error") {
+                          alert("error");
+                    }
                 }
-            }
-      });
+          });
+          }else{
+            // godziny nie są git
+            alert("Wrong availability format (example: 16:00 - 20:00");
+          }     
+        }
+      }else{
+        // numer jest źle
+        alert("Wrong number format");
+      }
+
+      
+
+     
+
+      
+
+      
+
+
+      
   });
 });
