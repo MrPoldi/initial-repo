7acda99209114dc28f3c9341e13ab83c38997a4c
MrPoldi <40165691+MrPoldi@users.noreply.github.com>
2020-01-13 22:30:03 +0100

    Poprawa zmiany hasła w JS

--- assets/js/kontojs.js
+++ assets/js/kontojs.js
@@ -1,17 +1,18 @@
 $(document).ready(function(){
   $("#passch-button").click(function(){      
 	  
+	  var myusername = $("#myusername").val();
 	  var password1 = $("#password1").val();
 	  var password2 = $("#password2").val();
 	  if(password1 == password2 && password1.length > 3 && password2.length > 3){
-		 var dataString = password1;
+      	  var dataString = "myusername=" + myusername + "&password1=" + password1 + "&password2=" + password2 + "&update=";
 	  }
 	  else{
 		 alert("Password is wrong. Required minimum 4 signs or same passwords in the brackets");
 	  }
       $.ajax({
             type: "POST",
-            url: "http://localhost/bikol/kontophp.php",
+            url: "https://bikolapka.000webhostapp.com/kontophp.php",
             data: dataString,
             crossDomain: true,
             cache: false,
@@ -20,11 +21,13 @@ $(document).ready(function(){
             },
             success: function(data) {
                 if (data == "success") {
-                    alert("inserted");
+                    alert("hasło zostało poprawnie zmienione");
                     $("#passch-button").val('Zmień hasło');
+		    window.location.href = 'https://bikolapka.000webhostapp.com/konto.html';
                 } 
                 else if (data == "error") {
-                       alert("error");
+                    alert("error");
+		    window.location.href = 'https://bikolapka.000webhostapp.com/konto.html';
                 }
             }
       });
