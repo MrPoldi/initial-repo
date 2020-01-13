1b20931e2ee43f2f25a39b2f4e02d194280d6b21
MrPoldi <40165691+MrPoldi@users.noreply.github.com>
2020-01-13 22:57:35 +0100

    Poprawa zmiany dostępności

--- php/ankietaphp.php
+++ php/ankietaphp.php
@@ -2,11 +2,10 @@
 include "db.php";
     if(isset($_POST['update']))
     {	
-		$password1 = $_POST['password1'];
-        $password2 = $_POST['password2'];
-		$myusername = $_POST['username'];
+	$availability = $_POST['availability'];
+	$myusername = $_POST['username'];
         
-        $q=mysqli_query($con,"UPDATE `contacts` ( `availability` ) SET 'availability' = '$availability' WHERE username = '$myusername'");
+        $q=mysqli_query($con,"UPDATE contacts SET availability = '$availability' WHERE nick = '$myusername'");
         if($q)
             echo "success";
         else
